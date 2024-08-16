734505bcf82e7f0778fa5327e8f1eaf655f2799b
Muhammad Mohsin <142692213+Volcann@users.noreply.github.com>
2024-08-17 03:39:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask ,request ,jsonify
 import pickle
-import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
@@ -11,11 +11,10 @@ def predict():
     iq = request.form.get('iq')
     profile_score = request.form.get('profile_score')
 
-    input_query = np.array([[cgpa,iq,profile_score]])
+    input_query = pd.DataFrame([[cgpa, iq, profile_score]], columns=['cgpa', 'iq', 'profile_score'])
     result = model.predict(input_query)[0]
 
     return jsonify({'result' : str(result)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
